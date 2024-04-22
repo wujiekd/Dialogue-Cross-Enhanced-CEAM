b69acc738d1fd833f60ab6347a9da124aa1731aa
wujie <lukeda@mail.ustc.edu.cn>
2024-04-22 14:50:24 +0800

    Update loss.py

--- src/loss.py
+++ src/loss.py
@@ -21,7 +21,7 @@ class CenterMSELoss(nn.Module):
 
         weighted_loss = torch.mean(mse_loss * weights) # explicitly
 
-        weighted_loss = weights / 3 * weighted_loss  # implicitly
+        weighted_loss = （self.beta+1+self.beta) / 3 * weighted_loss  # explicitly normalize or implicitly
 
         return weighted_loss
 
